9e14363008d529f7db4a4926b91611acb8206345
tasfia-alam <tasfiaalam547@gmail.com>
2025-10-01 20:42:13 +0600

    some changes

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // base: '/Taxi-Kitchen/',
   base: '/Taxi-Kitchen/',
   plugins: [react(), tailwindcss()],
 })
